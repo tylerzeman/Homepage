1f7b9d733c6d32ec2cf72138382c66a93d82b7e6
Connor Carciofini <ccarciofini@gmail.com>
2022-07-11 15:17:22 -0500

    added add string feature, some minor HTML tweaks in relation to said feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 M.AutoInit();
 
 $(".dropdown-trigger").dropdown();
+var containerEl = document.querySelector('.container');
+var newStringSubmitEl = document.getElementById('newStringSubmit');
+// addedStrings = JSON.parse(localStorage.getItem())
+newStringSubmitEl.addEventListener('click', addString);
+function addString(event) {
+    // event.preventDefault();
+    var newStringKeyInputEl = document.getElementById('newStringKeyInput');
+    var newStringValueInputEl = document.getElementById('newStringValueInput');
+    var newStringName = newStringKeyInputEl.value;
+    var newStringContent = newStringValueInputEl.value;
+    localStorage.setItem(newStringName, newStringContent);
+}
+for (i=0; i<localStorage.length; i++) {
+    newString = document.createElement('a');
+    newString.setAttribute('class', 'btn-floating btn-large waves-effect waves-light red');
+    newString.setAttribute('href', localStorage.getItem(localStorage.key(i)));
+    newString.innerHTML = localStorage.key(i)
+    newString.setAttribute('target', '_blank');
+    containerEl.appendChild(newString);
+}
