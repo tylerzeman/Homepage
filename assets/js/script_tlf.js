4eb163d816fd4cba7560792a9656fd0ca9f833cd
Anthony LoneFight <tonylonefight@gmail.com>
2022-07-13 14:49:43 -0500

    final pull

--- assets/js/script_tlf.js
+++ assets/js/script_tlf.js
@@ -1,15 +1,3 @@
 
-var colors = ['red', 'blue', 'green' ];
-
-if (colors.length < 2) {
-    console.log ('too short');
-} else if (colors.length === 2) {
-    console.log ('It equals 2');
-} else {
-console.log('default!');
-}   
-
-var btnEl = $('#linksbtn1').click (open(`https://google.com`, target) {
-});
 
 
